eecaf705003710b58fd835ab043889e97a3816c0
zhuman90 <45761113@qq.com>
2024-03-29 19:24:37 +0800

    .

---
+++ chapter06/web-push/server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  VAPIDKeys: {
+    publicKey: 'BC8Iccjh-zP7QQZWqjrVPiYtGCvruq3-wUfBqNlVaqPVHlEteiWUtFmGxcDKuWZssM4NldUx8yrvMScyw5cZcnU',
+    privateKey: 'g8M61bxa5VPhS-wS-6b83zgRLhowzS4Pp4qwKiilw4c'
+  },
+  GCMAPIkey: 'FCM Public Key'
+}
